f7d15a66052743ea28947d6c2a93ec685cf157b0
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-02-05 23:09:07 +0530

    updates

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Lander from './Lander';
+import PremiumLexWebsite from './PremiumLexWebsite';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<PremiumLexWebsite />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
